638a52740dba24cdb39f099f877dcef561e90675
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-01-23 11:49:47 +0630

    Add image skeleton loading

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import { formatPrice } from '../../utils/utils'
@@ -5,21 +6,29 @@ import styles from '../../styles/Product.module.css'
 
 export default function Product({ product }) {
     const router = useRouter()
+    const [imageLoaded, setImageLoaded] = useState(false)
 
     const viewProduct = () => {
         router.push(`/${product.id}`)
     }
 
+    const handleImageLoad = () => setImageLoaded(true)
+
     return (
         <div className={`col ${styles.product} text-center`}>
             <div className={styles['img-container']} onClick={viewProduct}>
                 <div className={styles.overlay}></div>
+
                 <Image
+                    id={product.id}
                     src={product.image}
                     alt={product.title}
-                    className='img-fluid ms-auto me-auto'
+                    className={`img-fluid ms-auto me-auto ${imageLoaded ? 'block' : 'hidden'}`}
                     layout='fill'
+                    onLoad={handleImageLoad}
                 />
+
+                {!imageLoaded && <div className={styles.skeleton} />}
             </div>
 
             <h2 className='mt-3 mt-lg-4' onClick={viewProduct}>
